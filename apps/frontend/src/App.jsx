b595f6ce79092e6dbf9366fe4aecae9629c0a2de
Pheewww <umangsingh67899876@gmail.com>
2024-06-18 01:24:04 +0530

    Project Setup Using Turborepo and Homescreen

---
+++ apps/frontend/src/App.jsx
@@ -0,0 +1,19 @@
+
+import './App.css'
+import HomeScreen from './Components/Homescreen';
+import Navbar from './Components/Navbar';
+
+function App() {
+
+  return (
+    <>
+      
+      <div><Navbar/></div>
+      <div>
+        <HomeScreen />
+      </div>
+    </>
+  );
+}
+
+export default App
