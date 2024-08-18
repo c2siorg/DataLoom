2bdc30fe02322394d62c2db0db4ffef765888919
Pheewww <umangsingh67899876@gmail.com>
2024-08-19 01:17:47 +0530

    first Demo

--- apps/frontend/src/Components/Homescreen.jsx
+++ apps/frontend/src/Components/Homescreen.jsx
@@ -1,5 +1,5 @@
 
-import { useState, useEffect } from "react";
+import React, { useState, useEffect } from "react";
 import { useNavigate } from "react-router-dom";
 import { uploadDataset, getRecentProjects, getDatasetDetails } from "../api.js";
 
