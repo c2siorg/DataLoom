976047fb454b424e0db2d7aa060bd6ef2abad1f0
Pheewww <umangsingh67899876@gmail.com>
2024-08-19 01:27:15 +0530

    rebase

--- apps/frontend/src/Components/Homescreen.jsx
+++ apps/frontend/src/Components/Homescreen.jsx
@@ -1,5 +1,5 @@
 
-import React, { useState, useEffect } from "react";
+import { useState, useEffect } from "react";
 import { useNavigate } from "react-router-dom";
 import { uploadDataset, getRecentProjects, getDatasetDetails } from "../api.js";
 
